c79d1c3332dc18ab45697ebe6063719919a0bda0
Гизбрехт Юлиан Александрович <gizbrekht.ya0796@okeit.edu>
2022-10-10 13:04:07 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Sum
+    {
+        int x;
+        int y;
+        int z;
+
+        public Sum(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public void PrintSum()
+        {
+            double r = x + y + z;
+            Console.WriteLine($"Задача №1: {x}+{y}+{z}\nРешение: {r}");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyLib;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum s = new Sum(1,2,3);
+            s.PrintSum();
+        }
+    }
+}
